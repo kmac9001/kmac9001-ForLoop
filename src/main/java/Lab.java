86cab464ea9a3352d42c9fca5d91539b8861f2e5
Kevin McCreary <kevinmccreary72@gmail.com>
2024-12-04 09:17:22 +0000

    all tests passed

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -43,8 +43,11 @@ public class Lab {
     public String returnNumbers(int start, int end){
         String buildingString = "";
 
-//        code here
+        for(int i = start; i < end; i++){
+            buildingString = buildingString + i + " ";
+        }
 
+        System.out.println(buildingString);
         return buildingString;
     }
 }
